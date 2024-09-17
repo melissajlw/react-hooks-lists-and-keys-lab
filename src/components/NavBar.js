84a951a326a4c829c5f60dd7dd18ac7da7868aff
Melissa Wang <melissa.wang21@gmail.com>
2024-09-16 20:05:34 -0400

    lists and keys completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,15 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const hrefLinks = links.map((link) => (
+    <a key={link} href={"#" + link}> {link} </a>
+  ))
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {hrefLinks}
+    </nav>
+  )
 }
 
 export default NavBar;
